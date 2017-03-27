c7e21362414894e943ee54eff533031d683678a3
Marcello Coutinho <marcellocoutinho@gmail.com>
2017-03-27 19:21:52 -0300

    updates for 2.3.x

--- mail/pfSense-pkg-postfix/files/usr/local/www/widgets/widgets/postfix.widget.php
+++ mail/pfSense-pkg-postfix/files/usr/local/www/widgets/widgets/postfix.widget.php
@@ -1,55 +1,88 @@
 <?php
 /*
-  		postfix.widget.php
-	    Copyright 2011-2014 Marcello Coutinho
-        Part of pfSense widgets (www.pfsense.org)
+ * postfix.widget.php
+ *
+ * part of pfSense (https://www.pfsense.org)
+ * Copyright (c) 2011-2017 Marcello Coutinho
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-        Redistribution and use in source and binary forms, with or without
-        modification, are permitted provided that the following conditions are met:
 
-        1. Redistributions of source code must retain the above copyright notice,
-           this list of conditions and the following disclaimer.
-
-        2. Redistributions in binary form must reproduce the above copyright
-           notice, this list of conditions and the following disclaimer in the
-           documentation and/or other materials provided with the distribution.
-
-        THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-        INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-        AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-        AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-        OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-        SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-        INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-        CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-        ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-        POSSIBILITY OF SUCH DAMAGE.
-*/
-@require_once("guiconfig.inc");
-@require_once("pfsense-utils.inc");
-@require_once("functions.inc");
+require_once("functions.inc");
+require_once("guiconfig.inc");
+require_once("pfsense-utils.inc");
+require_once("pkg-utils.inc");
+require_once("service-utils.inc");
 
 $uname=posix_uname();
 if ($uname['machine']=='amd64')
         ini_set('memory_limit', '250M');
 
-function open_table(){
-	echo "<table style=\"padding-top:0px; padding-bottom:0px; padding-left:0px; padding-right:0px\" width=\"100%\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">";
-	echo"  <tr>";
-}
-function close_table(){
-	echo"  </tr>";
-	echo"</table>";
+$dbc=array('bounced','deferred','hold','incoming','reject','spam','sent');
 
+function open_table($thead=""){
+	echo "<table border=1 class='table table-striped table-hover table-condensed'>\n";
+	echo "<thead><tr>".$thead."</tr></thread>";
+        echo "<tbody>\n";
+}
+
+function open_table_header($postfix_db){
+	global $dbc;
+	$h="<th style='text-align:center;'>Date</th>"; //print"<tr>";
+        foreach ($dbc as $c){
+        	$h .= "<th style='text-align:center;'>".ucfirst($c)."</th>";
+	}
+	open_table($h);
+}
+
+function close_table(){
+	echo"</tr>\n</tbody>";
+	echo"</table>";
+}
+
+function postfix_read_db($query,$file) {
+	$postfixdb = array();
+	$DB = postfix_opendb($file);
+	if ($DB) {
+		$response = $DB->query("{$query}");
+		if ($response != FALSE) {
+			while ($row = $response->fetchArray()) {
+				$postfixdb[] = $row;
+			}
+		} else {
+			print "Trying to read DB returned error: {$DB->lastErrorMsg()}";
+		}
+		$DB->close();
+	}
+	return $postfixdb;
+}
+
+function postfix_opendb($file) {
+	global $g,$postfix_dir,$postfix_arg;
+
+	$stm = "select id from mail_status;";
+	if (file_exists($postfix_dir.$file)) {
+		$DB = new SQLite3($postfix_dir.$file);
+		if ($DB->exec("{$stm};")) {
+			return $DB;
+		}
+	}
 }
 
 $pfb_table=array();
-$img['Sick']="<img src ='/themes/{$g['theme']}/images/icons/icon_interface_down.gif'>";
-$img['Healthy']="<img src ='/themes/{$g['theme']}/images/icons/icon_interface_up.gif'>";
 
-
-#var_dump($pfb_table);
-#exit;
 ?><div id='postfix'><?php
 global $config;
 
@@ -66,74 +99,85 @@ else
 
 $postfix_dir="/var/db/postfix/";
 $curr_time = time();
-for ($z = 0; $z > $days; $z--){
+$head_count=0;
+for ($z = 0; $z > $days; $z--) {
 
-if ($z==0)
+if ($z==0) {
 	$postfix_db=date("Y-m-d");
-else
+} else {
 	$postfix_db=date("Y-m-d",strtotime("$z day",$curr_time));
+}
 
-if (file_exists($postfix_dir.'/'.$postfix_db.".db")){
+if (file_exists($postfix_dir.'/'.$postfix_db.".db")) {
 	#noqueue
-	open_table();
-	print "<td class=\"vncellt\"><strong><center>$postfix_db</center></strong></td>";
-	close_table();
-	open_table();
-	if (@filesize($postfix_dir.'/'.$postfix_db.".db")< $size){
-		$dbhandle = sqlite_open($postfix_dir.'/'.$postfix_db.".db", 0666, $error);
+	if (@filesize($postfix_dir.'/'.$postfix_db.".db")< $size) {
 		$stm="select count(*) as total from mail_noqueue";
-		$result = sqlite_query($dbhandle, $stm);
-		$row_noqueue = sqlite_fetch_array($result, SQLITE_ASSOC);
+		$row_noqueue = postfix_read_db($stm,$postfix_db.".db");
 
-		#queue
-		$result = sqlite_query($dbhandle, $stm);
+		//queue
 		$stm="select mail_status.info as status,count(*) as total from mail_to,mail_status where mail_to.status=mail_status.id group by status order by mail_status.info";
-		$result = sqlite_query($dbhandle, $stm);
-		$reader="";
-		$count="";
-		for ($i = 1; $i <= 15; $i++) {
-					$row = sqlite_fetch_array($result, SQLITE_ASSOC);
-					 if (is_array($row)){
-					 	if (preg_match("/\w+/",$row['status'])){
-					 	$reader.="<td class=\"listlr\"width=50%><strong>".ucfirst($row['status'])."</strong></td>\n";
-					 	if ($row['status']=="reject")
-					 		$row['total']=+$row_noqueue['total'];
-						$count.="<td class=\"listlr\">".$row['total']."</td>\n";
-					 	}
-					 }
-					}
-		print "<tr>".$reader."</tr>";
-		print "<tr>".$count."</tr>";
-		$result = sqlite_query($dbhandle, $stm);
-		sqlite_close($dbhandle);
+		$result = postfix_read_db($stm,$postfix_db.".db");
+		foreach($dbc as $sc) {
+		$c[$sc]=0; //status count
 		}
-	else{
-		print "<td class=\"listlr\"width=100%><center>File size is too large.</center></td>";
+		foreach($result as $i => $row) {
+			if (is_array($row)) {
+				if (preg_match("/\w+/",$row['status'])) {
+					$c[$row['status']] = $row['total'];
+				 	if ($row['status']=="reject") {
+						$c[$row['status']]=+$row_noqueue[0]['total']; 
+				 	}
+				 }
+			}
 		}
-	close_table();
-	echo "<br>";
-
+		if(count($result) > 0) {
+			if ($head_count==0) {
+				open_table_header();
+				$head_count++;
+			}
+			echo"<tr><th style='text-align:center;'>{$postfix_db}</th>";
+			foreach($dbc as $sc) {
+				echo "<th style='text-align:right;'>" . number_format($c[$sc],0,"",".") . "</th>\n";
+			}
+			print "</tr>";
+		}
+	} else {
+		if ($head_count==0) {
+			open_table_header();
+			$head_count++;
+		}
+		$large_title="Database file is larger then widget max size($size).";
+		echo"<tr><th style='text-align:center;' title='{$large_title}'>{$postfix_db}</th>";
+		foreach($dbc as $sc) {
+			echo "<th style='text-align:right;' title='{$large_title}'>--</th>";
+		}
+		print "</tr>";
+	}
+  }
 }
-}
+close_table();
 echo"  </tr>";
 echo"</table></div>";
 
 ?>
+<script src="/vendor/jquery/jquery-1.12.0.min.js" type="text/javascript"></script>
 <script type="text/javascript">
-	function getstatus_postfix() {
-		var url = "/widgets/widgets/postfix.widget.php";
-		var pars = 'getupdatestatus=yes';
-		var myAjax = new Ajax.Request(
-			url,
-			{
-				method: 'get',
-				parameters: pars,
-				onComplete: activitycallback_postfix
-			});
+   function getstatus_postfix() {
+	var url = "/widgets/widgets/postfix.widget.php";
+	jQuery.ajax(url,
+		{
+		type: 'post',
+		data: {
+			getupdatestatus:  'yes'
+		},
+		success: function(ret){
+			$('#postfix').html(ret);
 		}
-	function activitycallback_postfix(transport) {
-		$('postfix').innerHTML = transport.responseText;
-		setTimeout('getstatus_postfix()', 60000);
-	}
-	getstatus_postfix();
+	});
+    }
+
+	$(document).ready(function() {
+		setTimeout(getstatus_postfix,10000);
+	});
+
 </script>
