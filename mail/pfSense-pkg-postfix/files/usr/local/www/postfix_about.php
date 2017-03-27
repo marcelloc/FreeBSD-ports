372f6e7b844deb619b5e07f32acdacadb5725a1a
Marcello Coutinho <marcellocoutinho@gmail.com>
2017-03-27 19:22:58 -0300

    updates for 2.3.x

--- mail/pfSense-pkg-postfix/files/usr/local/www/postfix_about.php
+++ mail/pfSense-pkg-postfix/files/usr/local/www/postfix_about.php
@@ -1,89 +1,62 @@
 <?php
 /*
-	postfix_about.php
-	part of pfSense (https://www.pfsense.org/)
-	Copyright (C) 2011-2015 Marcello Coutinho <marcellocoutinho@gmail.com>
-	based on varnish_view_config.
-	All rights reserved.
+ * postfix_about.php
+ *
+ * part of pfSense (https://www.pfsense.org)
+ * Copyright (c) 2011-2017 Marcello Coutinho
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+require_once("/etc/inc/util.inc");
+require_once("/etc/inc/functions.inc");
+require_once("/etc/inc/pkg-utils.inc");
+require_once("/etc/inc/globals.inc");
+require_once("guiconfig.inc");
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
-
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
-
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
-*/
+$pgtitle = array(gettext("Package"), gettext("Services: Postfix relay and antispam"), gettext("About"));
 $shortcut_section = "postfix";
-require("guiconfig.inc");
-
-$pf_version=substr(trim(file_get_contents("/etc/version")),0,3);
-if ($pf_version < 2.0)
-	$one_two = true;
-
-$pgtitle = "Diagnostics: Search Mail";
 include("head.inc");
 
+$tab_array = array();
+$tab_array[] = array(gettext("General"), false, "/pkg_edit.php?xml=postfix.xml&id=0");
+$tab_array[] = array(gettext("Domains"), false, "/pkg_edit.php?xml=postfix_domains.xml&id=0");
+$tab_array[] = array(gettext("Recipients"), false, "/pkg_edit.php?xml=postfix_recipients.xml&id=0");
+$tab_array[] = array(gettext("Access Lists"), false, "/pkg_edit.php?xml=postfix_acl.xml&id=0");
+$tab_array[] = array(gettext("Antispam"), false, "/pkg_edit.php?xml=postfix_antispam.xml&id=0");
+$tab_array[] = array(gettext("Sync"), false, "/pkg_edit.php?xml=postfix_sync.xml&id=0");
+$tab_array[] = array(gettext("View config"), false, "/postfix_view_config.php");
+$tab_array[] = array(gettext("Search mail"), false, "/postfix_search.php");
+$tab_array[] = array(gettext("Queue"), false, "/postfix_queue.php");
+$tab_array[] = array(gettext("About"), true, "/postfix_about.php");
+display_top_tabs($tab_array);
+
 ?>
-<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
-<?php include("fbegin.inc"); ?>
+<link rel="stylesheet" href="/vendor/datatable/css/jquery.dataTables.min.css">
+<div class="panel panel-default">
+        <div class="panel-heading"><h2 class="panel-title"><?=gettext("About Postfix Forwarder"); ?></h2></div>
+        <div class="panel-body">
+        <div class="table-responsive">
+		<table class="table table-hover table-condensed">
+                                <tbody>
 
-<?php if($one_two): ?>
-<p class="pgtitle"><?=$pgtitle?></font></p>
-<?php endif; ?>
-<?php if ($input_errors) print_input_errors($input_errors); ?>
-<?php if ($savemsg) print_info_box($savemsg); ?>
-
-<!-- <form action="postfix_view_config.php" method="post"> -->
-
-<div id="mainlevel">
-	<table width="100%" border="0" cellpadding="0" cellspacing="0">
-		<tr><td>
-		<?php
-	$tab_array = array();
-	$tab_array[] = array(gettext("General"), false, "/pkg_edit.php?xml=postfix.xml&id=0");
-	$tab_array[] = array(gettext("Domains"), false, "/pkg_edit.php?xml=postfix_domains.xml&id=0");
-	$tab_array[] = array(gettext("Recipients"), false, "/pkg_edit.php?xml=postfix_recipients.xml&id=0");
-	$tab_array[] = array(gettext("Access Lists"), false, "/pkg_edit.php?xml=postfix_acl.xml&id=0");
-	$tab_array[] = array(gettext("Antispam"), false, "/pkg_edit.php?xml=postfix_antispam.xml&id=0");
-	$tab_array[] = array(gettext("Sync"), false, "/pkg_edit.php?xml=postfix_sync.xml&id=0");
-	$tab_array[] = array(gettext("View config"), false, "/postfix_view_config.php");
-	$tab_array[] = array(gettext("Search mail"), false, "/postfix_search.php");
-	$tab_array[] = array(gettext("Queue"), false, "/postfix_queue.php");
-	$tab_array[] = array(gettext("About"), true, "/postfix_about.php");
-	display_top_tabs($tab_array);
-?>
-		</td></tr>
- 		<tr>
-
-    		<td>
-				<div id="mainarea">
-					<table class="tabcont" width="100%" border="0" cellpadding="8" cellspacing="0">
-					<tr><td></td></tr>
 						<tr>
-						<td colspan="2" valign="top" class="listtopic"><?=gettext("About Postfix Forwarder"); ?></td>
-						</tr>
-						<tr>
-                        <td width="22%" valign="top" class="vncell"><?=gettext("Credits ");?></td>
+                        <td width="22%" valign="top" class="vncell"><?=gettext("Credits");?>&nbsp;</td>
                         <td width="78%" class="vtable"><?=gettext("Package v2 Created by <a target=_new href='https://forum.pfsense.org/index.php?action=profile;u=4710'>Marcello Coutinho</a><br><br>");?></td>
                         </tr>
 						<tr>
-                        <td width="22%" valign="top" class="vncell"><?=gettext("Donatios ");?></td>
-                        <td width="78%" class="vtable"><?=gettext("If you like this package, please <a target=_new href='https://www.pfsense.org/index.php?option=com_content&task=view&id=47&Itemid=77'>donate to pfSense project</a>.<br><br>
-								 If you want that your donation goes to this package developer, make a note on donation forwarding it to me.<br><br>");?></td>
+                        <td width="22%" valign="top" class="vncell"><?=gettext("Donatios");?>&nbsp;</td>
+                        <td width="78%" class="vtable"><?=gettext("If you like this package, please donate for this community package developer.<br><br>");?></td>
                         </tr>
 						</table>
 
@@ -94,9 +67,6 @@ include("head.inc");
 
 	</table>
 	<br>
-	<div id="search_results"></div>
-</div>
-<!-- </form> -->
-<?php include("fend.inc"); ?>
+<?php include("foot.inc"); ?>
 </body>
 </html>
